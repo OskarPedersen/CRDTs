add133457561a306a0bbde7fb150ef722d55c48c
Oskar Pedersen <oskar.pedersen1@gmail.com>
2016-06-02 22:54:44 +0200

    Benchmark script

--- basic/benchmarks/generate_benchmarks.py
+++ basic/benchmarks/generate_benchmarks.py
@@ -23,9 +23,9 @@ for b in bodys:
     for t in bs_t:
         bs_outf = open('./generated/' + n + '-' + t + '.enc', 'w')
         res = bs_gen.replace('__SET__', t)
-        res = res.replace('__RUNS__', '1')
-        res = res.replace('__WORKERS__', '4')
-        res = res.replace('__OPERATIONS__', '300000')
+        res = res.replace('__RUNS__', '10')
+        res = res.replace('__WORKERS__', '8')
+        res = res.replace('__OPERATIONS__', '10000') #300000
         res = res.replace('__WAIT_GET__', bs_wg[i])
         res = res.replace('__BODY__', b)
         bs_outf.write(res)
--- basic/benchmarks/run.py
+++ basic/benchmarks/run.py
@@ -1,12 +1,23 @@
 from subprocess import call
 from os import listdir
 from os.path import isfile, join
+import sys
 #call(["rm -r generated/*"], shell=True)
 call(["python", "generate_benchmarks.py"])
 files = [f for f in listdir("generated") if isfile(join("generated", f)) and f[-4:] == ".enc"]
-#for f in files:
-    #call(["encorec","-I","..", "-o", "generated/" + f[:-4], "generated/" + f])
-
+files.sort()
 for f in files:
-    print(f)
+    call(["encorec","-I","..", "-o", "generated/" + f[:-4], "generated/" + f])
+names = ["Add Contains", "Add Remove Mixed", "Add Remove", "Add Same", "Add", "Few Add Contains"]
+i = 0
+for f in files:
+    if i % 4 == 0:
+        print("")
+        sys.stdout.write(names[i / 4])
+        sys.stdout.flush()
+    sys.stdout.write(" & ")
+    sys.stdout.flush()
     call(["generated/" + f[:-4]])
+    i += 1
+print("")
+print("")
