3b45998c4a467444a29e3ca90892834b90f4bdfd
Oskar Pedersen <oskar.pedersen1@gmail.com>
2016-06-03 16:15:40 +0200

    Ported traffic to new module system

--- basic/gen.py
+++ basic/gen.py
@@ -7,11 +7,11 @@ list_t = ['int', 'HashMapEntry_int_int', 'HashMapEntry_int_TimeOp',
 list_inf = open('gen_list.enc', 'r')
 list_gen = list_inf.read()
 list_inf.close()
-list_outf = open('list.enc', 'w')
-list_outf.write('bundle list where\n')
+outf = open('collections.enc', 'w')
+outf.write('bundle collections where\n')
+outf.write('import collections_data\n')
 for l in list_t:
-    list_outf.write(list_gen.replace('__TYPE__', l))
-list_outf.close()
+    outf.write(list_gen.replace('__TYPE__', l))
 
 
 hm_k =      ['int',     'int',          'int',                 'NodeID',        'NodeID',       'VertexPair',   'int',              'FromTo',       'FromTo',           'FromTo',               'FromTo',               'IDNext']
@@ -23,8 +23,8 @@ hm_v_null = ['-1',      'null : TimeOp','null : HashSet_int',  'null : RGANode',
 hm_inf = open('gen_hashmap.enc', 'r')
 hm_gen = hm_inf.read()
 hm_inf.close()
-hm_outf = open('hashmap.enc', 'w')
-hm_outf.write('bundle hashmap where\n')
+#hm_outf = open('hashmap.enc', 'w')
+#hm_outf.write('bundle hashmap where\n')
 i = 0
 for k in hm_k:
     v = hm_v[i]
@@ -38,10 +38,9 @@ for k in hm_k:
     res = res.replace('__TOINT__', t)
     res = res.replace('__KEY_NULL__', kn)
     res = res.replace('__VALUE_NULL__', vn)
-    hm_outf.write(res)
+    outf.write(res)
 
     i = i + 1
-hm_outf.close()
 
 
 
@@ -50,16 +49,16 @@ hs_null = ['-1', 'null', 'null', 'null','null']
 hs_inf = open('gen_hashset.enc', 'r')
 hs_gen = hs_inf.read()
 hs_inf.close()
-hs_outf = open('hashset.enc', 'w')
-hs_outf.write('bundle hashset where\n')
+#hs_outf = open('hashset.enc', 'w')
+#hs_outf.write('bundle hashset where\n')
 i = 0
 for t in hs_t:
     n = hs_null[i]
     res = hs_gen.replace('__TYPE__', t)
     res = res.replace('__NULL__', n)
-    hs_outf.write(res)
+    outf.write(res)
     i = i + 1
-hs_outf.close()
+outf.close()
 
 
 t_set = ['ActiveHashMap_FromTo_int', 'SubMap', 'DivTimeMap', 'OpOrMap']
